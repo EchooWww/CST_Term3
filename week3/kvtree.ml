93c66c8925eb76c5140eb18e17f744def38bafe2
Echo Wang <ywang623@my.bcit.ca>
2024-10-02 16:03:03 -0700

    Modify delete node in kvtree

--- week3/kvtree.ml
+++ week3/kvtree.ml
@@ -31,34 +31,39 @@ let rec find k t =
   | Node ((xkey, xval), l, r) when k = xkey -> Some(xval)
   | _ -> None
 
-(** [largest] returns the value associated with the largest key in [t]. *)
+(** [largest] returns the largest key in [t]. *)
 let rec largest t =
   match t with 
   | Leaf -> failwith "largest: empty tree"
-  | Node ((k, v),_, Leaf) -> k
+  | Node ((k, v),_, Leaf) -> (k,v)
   | Node(_, _, r)  -> largest r
 
-(** [smallest] returns the value associated with the smallest key in [t]. *)
+(** [smallest] returns the smallest key in [t]. *)
 let rec smallest t =
   match t with
   | Leaf -> failwith "smallest: empty tree"
-  | Node ((k, v), Leaf, _) -> k
+  | Node ((k, v), Leaf, _) -> (k,v)
   | Node (_, l, _) -> smallest l
 
+(** [delete] returns a new KV tree with key [k] removed from [t]. *)
 (** [delete] returns a new KV tree with key [k] removed from [t]. *)
 let rec delete k t =
   match t with
   | Leaf -> Leaf
-  | Node ((xkey,xval), l, r) when k < xkey ->
+  | Node ((xkey, xval), l, r) when k < xkey ->
     Node((xkey, xval), delete k l, r)
-  | Node ((xkey,xval), l, r) when k > xkey ->
-    Node ((xkey,xval), l, delete k r)
+  | Node ((xkey, xval), l, r) when k > xkey ->
+    Node ((xkey, xval), l, delete k r)
   | Node (_, Leaf, r) -> r
   | Node (_, l, Leaf) -> l
-  | Node ((xkey,xval), l, r)->
-    if (largest l) > (smallest r) 
-    then Node((largest l, xval), delete (largest l) l, r)
-    else Node((smallest r, xval), l, delete (smallest r) r)
+  | Node ((xkey, xval), l, r) ->
+    let (largest_key_left, val_with_largest_key) = largest l in
+    let (smallest_key_right, val_with_smallest_key) = smallest r in
+    if largest_key_left > smallest_key_right then
+      Node ((largest_key_left, val_with_largest_key), delete largest_key_left l, r)
+    else
+      Node ((smallest_key_right, val_with_smallest_key), l, delete smallest_key_right r)
+
 
 (** [of_list] returns a new KV tree with key-value pairs from [lst]. *)
 let of_list lst =
